5a1d47caebcdeeb696bb6e2f9db9f14402e53aed
Reza Pishva <rpishva@gmail.com>
2023-05-25 22:22:46 +0330

    add some docs to controllers

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue3PersianDatetimePicker from 'vue3-persian-datetime-picker'
+import router from './router1.js'
+const app = createApp(App)
+app.use(router)
+app.component('DatePicker', Vue3PersianDatetimePicker)
+app.mount('#app')
+
+
+
+
+
