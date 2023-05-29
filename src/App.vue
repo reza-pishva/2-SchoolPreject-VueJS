b636c9049c5beebc2bd9adf5d6988cb8c0108f62
Reza Pishva <rpishva@gmail.com>
2023-05-29 13:11:29 +0330

    added components of app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <HomeComponent />
-  <router-view> </router-view>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -11,88 +11,9 @@ export default {
   components: {
     HomeComponent,
   },
-  data() {
-    return {
-      report_title: [],
-      Eq: false,
-      Pr: true,
-    };
-  },
+  data() {},
   methods: {},
 };
 </script>
 
-<style>
-.menu_text {
-  font-family: Vazir;
-  color: white;
-  font-size: 14px;
-  text-align: right;
-  margin-top: 1px;
-}
-
-.menu_effect:hover {
-  height: 35px;
-  padding-top: 4px;
-  border-radius: 5px;
-  opacity: 1;
-  filter: alpha(opacity=100);
-  outline: 2px solid white;
-  outline-offset: 1px;
-  cursor: pointer;
-  background-color: darkkhaki;
-  color: black;
-}
-.menu_effect {
-  height: 35px;
-  padding-top: 4px;
-  color: white;
-  border-radius: 5px;
-  opacity: 0.7;
-  filter: alpha(opacity=100);
-}
-
-.slide-fade-enter-active {
-  transition: all 0.3s ease-out;
-}
-
-.slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(20px);
-  opacity: 0;
-}
-
-/* .kala:hover {
-  opacity: 1;
-  filter: alpha(opacity=100);
-  outline: 2px solid white;
-  outline-offset: 1px;
-  cursor: pointer;
-  border-radius: 5px;
-} */
-.kala {
-  opacity: 0.7;
-  filter: alpha(opacity=100);
-}
-
-/* .afrad:hover {
-  opacity: 1;
-  filter: alpha(opacity=100);
-  outline: 2px solid white;
-  outline-offset: 1px;
-  cursor: pointer;
-  border-radius: 5px;
-} */
-.afrad {
-  opacity: 0.7;
-  filter: alpha(opacity=100);
-}
-@font-face {
-  font-family: "Vazir";
-  src: local("Vazir"), url(./fonts/Vazir.ttf) format("truetype");
-}
-</style>
+<style></style>
