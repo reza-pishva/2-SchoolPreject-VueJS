ae8ec604f1d464d17d67cfa248bb1d0d5004fe3c
Reza Pishva <rpishva@gmail.com>
2023-05-31 08:11:14 +0330

    added components of app.vue

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,17 +1,14 @@
 <template>
   <TopBaner />
-  <!-- <UserComponent /> -->
 </template>
 
 <script>
 import TopBaner from "./FirstPageComponents/TopBaner.vue";
-//import UserComponent from "./UserComponents/UserGetComponent.vue";
 
 export default {
   name: "App",
   components: {
     TopBaner,
-    // UserComponent,
   },
   data() {},
   methods: {},
