3a2b2e9f360da96efec4525fae49768903865775
Reza Pishva <rpishva@gmail.com>
2023-06-07 10:51:45 +0330

    added styles to forms

--- src/components/SettingComponents/ExamTypeComponents/ExamTypeInsertFormComponent.vue
+++ src/components/SettingComponents/ExamTypeComponents/ExamTypeInsertFormComponent.vue
@@ -204,7 +204,7 @@ export default {
   height: 35px;
 }
 .exam-type-window {
-  background-color: rgb(146, 204, 233);
+  background-color: rgb(89, 99, 133);
   margin-left: 30px;
   height: 95%;
   border-radius: 6px;
--- src/components/SettingComponents/GradeComponents/GradeInsertFormComponent.vue
+++ src/components/SettingComponents/GradeComponents/GradeInsertFormComponent.vue
@@ -201,7 +201,7 @@ export default {
   height: 30px;
 }
 .grade-window {
-  background-color: rgb(146, 204, 233);
+  background-color: rgb(89, 99, 133);
   margin-left: 30px;
   height: 95%;
   border-radius: 6px;
