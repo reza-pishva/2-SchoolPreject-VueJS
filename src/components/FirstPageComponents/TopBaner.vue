3ec6c8d3c095a2907e1763a6b86907a904614a0a
Reza Pishva <rpishva@gmail.com>
2023-06-09 14:14:36 +0330

    added styles to forms

--- src/components/FirstPageComponents/TopBaner.vue
+++ src/components/FirstPageComponents/TopBaner.vue
@@ -1,62 +1,79 @@
 <template>
   <div
-    class="sticky"
+    class="row sticky"
     style="
       border-radius: 5px;
-      background-image: url('report_bg.webp');
+      /* background-image: url('report_bg.webp'); */
       margin: auto;
-      width: 21%;
+      width: 100%;
       height: 135px;
       margin-right: 20px;
     "
   >
-    <div class="row" style="padding-top: 10px">
-      <div
-        class="col-4"
-        style="height: 165px; text-align: center; padding-top: 15px"
-      >
-        <router-link to="/personels">
-          <img
-            src="../../../public/admin.png"
-            style="height: 40%; width: 75%; border-radius: 5px"
-          />
-        </router-link>
-        <div style="padding-top: 10px">
-          <p style="font-family: Vazir; font-size: 13px; color: aliceblue">
-            کارکنان
-          </p>
+    <div class="col-4 mt-2">
+      <img
+        src="../../../public/idealparsi.png"
+        style="height: 75%; width: 30%; border-radius: 5px; margin-left: -100px"
+      />
+    </div>
+    <div class="col-5"></div>
+    <div
+      class="col-3"
+      style="
+        padding-top: 10px;
+        background-image: url('report_bg.webp');
+        height: 95%;
+        border-radius: 10px;
+      "
+    >
+      <div class="row">
+        <div
+          class="col-4"
+          style="height: 165px; text-align: center; padding-top: 15px"
+        >
+          <router-link to="/personels">
+            <img
+              src="../../../public/admin.png"
+              style="height: 40%; width: 75%; border-radius: 5px"
+            />
+          </router-link>
+          <div style="padding-top: 10px">
+            <p style="font-family: Vazir; font-size: 13px; color: aliceblue">
+              کارکنان
+            </p>
+          </div>
         </div>
-      </div>
-      <div
-        class="col-4"
-        style="height: 165px; text-align: center; padding-top: 15px"
-      >
-        <router-link to="/students">
-          <img
-            src="../../../public/student.jpg"
-            style="height: 40%; width: 75%; border-radius: 5px"
-          />
-        </router-link>
-        <div style="padding-top: 10px">
-          <p style="font-family: Vazir; font-size: 13px; color: aliceblue">
-            دانش آموز
-          </p>
+        <div
+          class="col-4"
+          style="height: 165px; text-align: center; padding-top: 15px"
+        >
+          <router-link to="/students">
+            <img
+              src="../../../public/student.jpg"
+              style="height: 40%; width: 75%; border-radius: 5px"
+            />
+          </router-link>
+          <div style="padding-top: 10px">
+            <p style="font-family: Vazir; font-size: 13px; color: aliceblue">
+              دانش آموز
+            </p>
+          </div>
         </div>
-      </div>
-      <div
-        class="col-4"
-        style="height: 165px; text-align: center; padding-top: 15px"
-      >
-        <router-link to="/home">
-          <img
-            src="../../../public/main.png"
-            style="height: 40%; width: 75%; border-radius: 5px"
-          />
-        </router-link>
-        <div style="padding-top: 10px">
-          <p style="font-family: Vazir; font-size: 13px; color: aliceblue">
-            صفحه اصلی
-          </p>
+        <div
+          class="col-4"
+          style="height: 165px; text-align: center; padding-top: 15px"
+        >
+          <router-link to="/home">
+            <img
+              src="../../../public/main.png"
+              style="height: 40%; width: 75%; border-radius: 5px"
+            />
+          </router-link>
+          <div style="padding-top: 10px">
+            <p style="font-family: Vazir; font-size: 13px; color: aliceblue">
+              صفحه اصلی
+            </p>
+          </div>
         </div>
       </div>
     </div>
