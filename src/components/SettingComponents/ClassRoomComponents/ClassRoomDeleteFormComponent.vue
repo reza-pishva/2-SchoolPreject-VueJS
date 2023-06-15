5d8873bfcb7592dfd37d5e9d0d3d4c40ed57e295
Reza Pishva <rpishva@gmail.com>
2023-06-15 10:04:12 +0330

    added form components

--- src/components/SettingComponents/ClassRoomComponents/ClassRoomDeleteFormComponent.vue
+++ src/components/SettingComponents/ClassRoomComponents/ClassRoomDeleteFormComponent.vue
@@ -12,12 +12,12 @@
           padding-right: 5px;
         "
       >
-        <!-- <ClassRoomForm
-          @class_id="deleteClass"
+        <ClassRoomForm
+          @deleteId="deleteClass"
           :button-loading="loading"
           button-text="حذف"
           button-class="btn btn-danger"
-        /> -->
+        />
       </div>
     </div>
   </div>
@@ -28,40 +28,68 @@ import { ref } from "vue";
 import { useRoute } from "vue-router";
 import axios from "axios";
 import Swal from "sweetalert2";
-// import ClassRoomForm from "@/components/Forms/ClassRoomFormComponent.vue";
+import ClassRoomForm from "@/components/Forms/ClassRoomFormComponent.vue";
 
 export default {
-  // components: {
-  //   ClassRoomForm,
-  // },
+  components: {
+    ClassRoomForm,
+  },
   setup() {
     const loading = ref(false);
     const route = useRoute();
 
     function deleteClass() {
-      axios
-        .delete(
-          `http://127.0.0.1:8000/api/school/classroom/remove/${route.params.id}`
-        )
-        .then(function () {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+        position: "top",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axios
+            .delete(
+              `http://127.0.0.1:8000/api/school/classroom/remove/${route.params.id}`
+            )
+            .then(function () {})
+            .catch(function (error) {
+              console.log(error);
+            });
           Swal.fire({
             title: "Thanks!",
             text: `کلاس با کد (${route.params.id}) با موفقیت حذف گردید`,
-            icon: "warning",
+            icon: "success",
             confirmButtonText: "Ok",
             position: "top",
           });
-        })
-        .catch(function (error) {
-          console.log(error);
-          Swal.fire({
-            title: "پیغام خطا",
-            text: "مشکلاتی در مورد ثبت اطلاعات در پایگاه داده مشاهده گردیده",
-            icon: "error",
-            confirmButtonText: "Ok",
-            position: "top",
-          });
-        });
+        }
+      });
+      // axios
+      //   .delete(
+      //     `http://127.0.0.1:8000/api/school/classroom/remove/${route.params.id}`
+      //   )
+      //   .then(function () {
+      //     Swal.fire({
+      //       title: "Thanks!",
+      //       text: `کلاس با کد (${route.params.id}) با موفقیت حذف گردید`,
+      //       icon: "warning",
+      //       confirmButtonText: "Ok",
+      //       position: "top",
+      //     });
+      //   })
+      //   .catch(function (error) {
+      //     console.log(error);
+      //     Swal.fire({
+      //       title: "پیغام خطا",
+      //       text: "مشکلاتی در مورد ثبت اطلاعات در پایگاه داده مشاهده گردیده",
+      //       icon: "error",
+      //       confirmButtonText: "Ok",
+      //       position: "top",
+      //     });
+      //   });
     }
     deleteClass();
 
